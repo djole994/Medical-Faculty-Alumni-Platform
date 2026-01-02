0b26abe49a070ac81ea3e004030f7b11dfdd9a19
Djordje Radovic <120112255+djole994@users.noreply.github.com>
2026-01-02 14:47:47 +0100

    Add AlumniProfile model with properties

--- backend/src/AlumniApi/Models/AlumniProfile.cs
+++ backend/src/AlumniApi/Models/AlumniProfile.cs
@@ -1,1 +1,18 @@
+public class AlumniProfile
+{
+    public int Id { get; set; }
 
+    public string FullName { get; set; } = "";
+    public string ContactEmail { get; set; } = "";
+
+    public string City { get; set; } = "";
+    public int CountryId { get; set; }
+
+    public double? Latitude { get; set; }
+    public double? Longitude { get; set; }
+    public bool IsLocationVerified { get; set; }
+    public int? GeoCacheId { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public bool IsApproved { get; set; }
+}
