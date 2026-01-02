8879ae40233f61bb524979e7510c9171d0c8d567
Djordje Radovic <120112255+djole994@users.noreply.github.com>
2026-01-02 14:26:58 +0100

    Add HttpClient configuration for Geocoding service

--- backend/src/AlumniApi/Program.cs
+++ backend/src/AlumniApi/Program.cs
@@ -1,1 +1,4 @@
-
+builder.Services.AddHttpClient<IGeocodingService, Geocoding>(client =>
+{
+    client.DefaultRequestHeaders.UserAgent.ParseAdd("AlumniApp/1.0 (contact: true_email@gmail.com)");
+});
